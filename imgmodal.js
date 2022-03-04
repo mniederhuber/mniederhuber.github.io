44004ce07365eedbf4ad481be73512eed774c819
mniederhuber <m@wanda.wireless-1x.unc.edu>
2022-03-04 12:19:43 -0500

    updating with most recent stylesheet index and adding *.js files

---
+++ imgmodal.js
@@ -0,0 +1,22 @@
+// Get the modal
+var modal = document.querySelector('.myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.querySelector('.myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
